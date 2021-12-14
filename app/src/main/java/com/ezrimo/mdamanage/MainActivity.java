81e399df76a86c43a827c25a92405180ae8f8cc4
ezrim <ezrimo@gmail.com>
2021-12-14 23:38:49 +0200

    start of sing up/in desing

--- app/src/main/java/com/ezrimo/mdamanage/MainActivity.java
+++ app/src/main/java/com/ezrimo/mdamanage/MainActivity.java
@@ -2,13 +2,27 @@ package com.ezrimo.mdamanage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_SCREEN=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent go = new Intent(MainActivity.this, SignInUpActivity.class);
+                startActivity(go);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
     }
 }
---
+++ app/src/main/java/com/ezrimo/mdamanage/SignInUpActivity.java
@@ -0,0 +1,14 @@
+package com.ezrimo.mdamanage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_up);
+    }
+}
